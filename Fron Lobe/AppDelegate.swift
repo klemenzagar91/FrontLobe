214e9b3e9d53e7fa0a7adb5d258ae511ac6423a2
Klemen Zagar <klemen.zagar91@gmail.com>
2017-02-22 18:21:37 +0100

    Initial Commit

---
+++ Fron Lobe/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Fron Lobe
+//
+//  Created by Klemen Zagar on 2/22/17.
+//  Copyright © 2017 Klemen Zagar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
