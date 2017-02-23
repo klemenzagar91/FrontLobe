6cc0406a8a2dd23b462d330d9406ca004f67d1a9
Klemen Zagar <klemen.zagar91@gmail.com>
2017-02-23 17:51:24 +0100

    no message

---
+++ Fron Lobe/Controller/MainVC.swift
@@ -0,0 +1,86 @@
+//
+//  MainVC.swift
+//  Fron Lobe
+//
+//  Created by Klemen Zagar on 2/22/17.
+//  Copyright © 2017 Klemen Zagar. All rights reserved.
+//
+
+import Cocoa
+
+class MainVC: NSViewController, DestinationViewDelegate {
+  @IBOutlet var dropView: DropProjectView!
+  @IBOutlet weak var projectPathLabel: NSTextField!
+  var projectURL: URL? {
+    didSet {
+      projectPathLabel.stringValue = projectURL?.path ?? ""
+    }
+  }
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      dropView.delegate = self
+      
+        // Do view setup here.
+    }
+  
+  @IBAction func handleCreateGemfileButton(_ sender: NSButton) {
+    print("hello")
+    if let projectURL = projectURL {
+      runScript(["cd", projectURL.path]) {
+        print("step 1")
+        self.runScript(["-c", "bundle", "init"], handler: {
+          print("step 2")
+        })
+      }
+//      let pipe = Pipe()
+//      let process = Process()
+//      process.launchPath = "/bin/bash"
+//      process.arguments = ["cd", projectURL.path]
+//      process.standardOutput = pipe
+//      let file = pipe.fileHandleForReading
+//      process.launch()
+//      
+//      
+//      if let result = String(data: file.readDataToEndOfFile(), encoding: String.Encoding.utf8) {
+//        print(result)
+//      }
+//      else {
+//        print("error")
+//      }
+    }
+  }
+  
+  
+  func runScript(_ arguments:[String], handler:@escaping () -> ()) {
+    let taskQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
+    taskQueue.async {
+      let buildTask = Process()
+      buildTask.launchPath = "/bin/bash"
+      buildTask.arguments = arguments
+      
+      buildTask.terminationHandler = {
+        task in
+        DispatchQueue.main.async(execute: {
+          handler()
+        })
+      }
+      buildTask.launch()
+      buildTask.waitUntilExit()
+    }
+    
+  }
+  
+  func processImage(_ image: NSImage, center: NSPoint) {
+
+  }
+  
+  func processImageURLs(_ url: URL, center: NSPoint) {
+    print("hello", url);
+    projectURL = url
+  }
+  
+  func processAction(_ action: String, center: NSPoint) {
+    
+  }
+}
---
+++ Fron Lobe/View/DropProjectView.swift
@@ -0,0 +1,99 @@
+//
+//  DropProjectView.swift
+//  Fron Lobe
+//
+//  Created by Klemen Zagar on 2/22/17.
+//  Copyright © 2017 Klemen Zagar. All rights reserved.
+//
+
+import Cocoa
+
+protocol DestinationViewDelegate {
+  func processImageURLs(_ url: URL, center: NSPoint)
+  func processImage(_ image: NSImage, center: NSPoint)
+  func processAction(_ action: String, center: NSPoint)
+}
+
+class DropProjectView: NSView {
+  let filteringOptions = [NSPasteboardURLReadingContentsConformToTypesKey: kUTTypeFolder]
+  var acceptableTypes: Set<String> { return [NSURLPboardType] }
+  var delegate: DestinationViewDelegate?
+  
+  func setup() {
+    register(forDraggedTypes: Array(acceptableTypes))
+  }
+  
+  override func awakeFromNib() {
+    setup()
+  }
+  
+  override func draw(_ dirtyRect: NSRect) {
+    
+    if isReceivingDrag {
+      NSColor.selectedControlColor.set()
+      
+      let path = NSBezierPath(rect:bounds)
+      path.lineWidth = 5.0
+      path.stroke()
+    }
+  }
+  
+  var isReceivingDrag = false {
+    didSet {
+      needsDisplay = true
+    }
+  }
+  
+  
+  
+  // Dragging
+  func shouldAllowDrag(_ draggingInfo: NSDraggingInfo) -> Bool {
+    
+    var canAccept = false
+    
+    //2.
+    let pasteBoard = draggingInfo.draggingPasteboard()
+    
+    //3.
+    if pasteBoard.canReadObject(forClasses: [NSURL.self]) {
+      canAccept = true
+    }
+    return canAccept
+    
+  }
+  
+  override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+    let allow = shouldAllowDrag(sender)
+    isReceivingDrag = allow
+    return allow ? .copy : NSDragOperation()
+  }
+  
+  override func draggingExited(_ sender: NSDraggingInfo?) {
+    isReceivingDrag = false
+  }
+  
+  override func prepareForDragOperation(_ sender: NSDraggingInfo) -> Bool {
+    let allow = shouldAllowDrag(sender)
+    return allow
+  }
+  
+  override func performDragOperation(_ draggingInfo: NSDraggingInfo) -> Bool {
+    
+    isReceivingDrag = false
+    let pasteBoard = draggingInfo.draggingPasteboard()
+    
+    //2.
+    let point = convert(draggingInfo.draggingLocation(), from: nil)
+    //3.
+    if let urls = pasteBoard.readObjects(forClasses: [NSURL.self]) as? [URL], urls.count > 0 {
+      if let url = urls.first {
+        delegate?.processImageURLs(url, center: point)
+        return true
+      }
+      
+    }
+    return false
+    
+  }
+    
+}
